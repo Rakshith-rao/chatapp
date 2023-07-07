ce1888570efdb5f9c5dd9d53c165ea5ce540f90d
Rakshith-rao <121076038+Rakshith-rao@users.noreply.github.com>
2023-07-07 12:33:40 +0530

    Add files via upload

---
+++ chatgpt.py
@@ -0,0 +1,42 @@
+import os
+from dotenv import load_dotenv
+import streamlit as st
+from streamlit_chat import message
+from utils import get_initial_message, get_chatgpt_response, update_chat
+import openai
+
+load_dotenv()
+
+openai.api_key = os.getenv('OPENAI_API_KEY')
+openai.api_key  = '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'
+st.title("Chatbot")
+st.subheader("The ultimate AI assistance")
+
+model = "gpt-3.5-turbo"
+
+if 'generated' not in st.session_state:
+    st.session_state['generated'] = []
+if 'past' not in st.session_state:
+    st.session_state['past'] = []
+
+query = st.text_input("Question:", key="input")
+
+if 'messages' not in st.session_state:
+    st.session_state['messages'] = get_initial_message()
+
+if query:
+    with st.spinner("generating..."):
+        messages = st.session_state['messages']
+        messages = update_chat(messages, "user", query)
+        response = get_chatgpt_response(messages, model)
+        messages = update_chat(messages, "assistant", response)
+        st.session_state.past.append(query)
+        st.session_state.generated.append(response)
+
+if st.session_state['generated']:
+    for i in range(len(st.session_state['generated'])-1, -1, -1):
+        message(st.session_state['past'][i], is_user=True, key=str(i) + '_user')
+        message(st.session_state["generated"][i], key=str(i))
+
+    with st.expander("Show Messages"):
+        st.write(messages)
